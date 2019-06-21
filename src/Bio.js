ca4f04fc2e5f9975abe6f2a3a5e0a45a246bec8f
kyliesanderson <kyliesanderson@utexas.edu>
2019-06-20 23:51:06 -0500

    biography page

--- src/Bio.js
+++ src/Bio.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
+import {Col, Row} from 'react-bootstrap';
 import Header from './Header.js';
+import content from './content.json';
+import bioPic from './biography-pic.jpg';
 
 class Bio extends Component {
+
+   constructor(props){
+      super(props);
+
+      this.render = this.render.bind(this);
+      this.generateBio = this.generateBio.bind(this);
+      this.generateParagraph = this.generateParagraph.bind(this);
+   }
+
+   generateParagraph(paragraph){
+      return (
+         <p className="bio-text">{paragraph}</p>
+      );
+   }
+
+   generateBio(paragraphs){
+      return paragraphs.map(this.generateParagraph);
+   }
 
    render(){
       return(
          <div>
             <Header page="bio" />
+            <Row>
+               <Col md={7} className="bio-container">
+                  {this.generateBio(content.longBio)}
+               </Col>
+               <Col md={3} className="pic-container">
+                  <img className="bio-pic" src={bioPic} />
+               </Col>
+            </Row>
          </div>
       );
    }
